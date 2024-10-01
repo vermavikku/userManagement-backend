29479772d370b5b8b0c222e492051c1e676f4c14
Vikas Verma <vikas.verma@techplusinfotech.com>
2024-10-01 14:37:07 +0530

    updated

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const db = require("./src/config/database");
 require("dotenv").config();
 const routes = require("./src/routes");
 
-const app = express();
-
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
@@ -22,5 +14,5 @@ app.use("/v1", routes);
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });

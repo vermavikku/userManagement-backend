4d1b2fbb0bf457afcd8f4f7fe9091d08f6f4cf0b
Vikas Verma <vikas.verma@techplusinfotech.com>
2024-10-01 13:04:25 +0530

    up

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const db = require("./src/config/database");
 require("dotenv").config();
 const routes = require("./src/routes");
 
-const corsOptions = {
-  origin:
-    "https://66fba2f2ff7b0bc9477d571e--comforting-babka-d6dd19.netlify.app/", // Allow only your front-end URL
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all methods
-  credentials: true, // Allow credentials if needed (like cookies)
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
